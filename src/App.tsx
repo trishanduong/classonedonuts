ddcf58cfc2249e712485313fdf42f51d8ff6e097
trisha <trishanduong@gmail.com>
2024-01-18 20:07:59 -0800

    FavIcon and hero mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from "react";
+
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 
-
 function App() {
+
 
   return (
     <>
       <Navigation />
       <div className="h-screen">
         <Hero />
-        <div className="h-screen bg-ppink flex justify-center items-center">
-          hello steven
-        </div>
+        {/* <div className="h-auto bg-dbrown flex justify-center items-center">
+          <p className="font-grotesk text-5xl lg:text-7xl font-medium text-pbrown p-6">From our family to yours, blending tradition with innovation.♥</p>
+        </div> */}
       </div>
     </>
   )
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = ( ) => {
     const tl = gsap.timeline();
     tl
       .to(imgContainer.current, { opacity:80, duration: 1.5, ease: "power4.inOut" } )
-      .to(text.current, { y: 0, duration: 1, ease: "power4.inOut", stagger:1 }, "-=1.0")
+      .to(text.current, { y: 0, opacity:1, duration: 1, ease: "power4.inOut", stagger:1 }, "-=1.0")
       .to(order.current, { y: 0, opacity:1, duration: 2, ease: "power4.inOut" }, "-=1.0")
   }, []);
 
@@ -39,11 +39,11 @@ const Hero = ( ) => {
     <div className="hero relative flex w-full h-screen select-none justify-center items-center" aria-label="hero">
       <div ref={imgContainer} className="relative opacity-0 overflow-hidden scale-100 h-screen w-full">
         <img ref={img} src={Landing} alt="Assortment of donuts placed next to a luscious pothos plant." className="scale-100 w-full h-full object-cover lg:w-screen lg:overflow-auto opacity-90 pointer-none" />
-        <button ref={order} className="translate-y-96 absolute lg:top-24 lg:right-24 bg-beige-white text-pbrown font-grotesk font-semibold text-3xl px-4 py-2 rounded-sm shadow-lg hover:bg-dbrown hover:text-zinc-100 hover:transition-transform opacity-0">
+        <button ref={order} className="translate-y-96 absolute bottom-24 right-0 bg-beige-white text-pbrown font-grotesk font-semibold text-3xl px-4 py-2 rounded-full shadow-lg hover:bg-dbrown hover:text-zinc-100 hover:transition-transform opacity-0 border border-dbrown hover:border-beige-white">
             Order Now
         </button>
-        <div className="hidden lg:absolute lg:block lg:top-1/2">
-            <div ref={text} className="translate-y-96 p-3 text-beige-white text-5xl lg:text-8xl drop-shadow-md w-1/2">
+        <div className="absolute top-4 left-44 lg:left-0 lg:top-1/2">
+            <div ref={text} className="translate-y-96 p-3 text-beige-white text-5xl lg:text-8xl drop-shadow-md lg:w-1/2 opacity-0">
                 voted the <span className="italic text-dpink bg-beige-white px-3">best</span> donuts in glendora
             </div>
         </div>
